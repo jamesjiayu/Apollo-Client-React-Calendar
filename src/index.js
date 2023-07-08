8ea12e80d18981167d2a5fcbc9553b66b1cf223a
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-07-08 11:14:23 -0500

    1 file modified

--- src/index.js
+++ src/index.js
@@ -16,20 +16,20 @@ const client = new ApolloClient({
   uri: 'https://flyby-router-demo.herokuapp.com/',
   cache: new InMemoryCache(),
 });
-// client
-//   .query({
-//     query: gql`
-// query GetLocations{
-//   locations{
-//     id
-//     name
-//     description
-//     photo
-//   }
-// }
-// `,
-//   })
-//   .then((result) => console.log(result));
+client
+  .query({
+    query: gql`
+query GetLocations{
+  locations{
+    id
+    name
+    description
+    photo
+  }
+}
+`,
+  })
+  .then((result) => console.log(result));
 createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
     <App />
