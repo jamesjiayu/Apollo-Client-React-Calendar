93cd1e0d96a6e1c58166071a4925d14f1ec99ca3
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-07-20 14:01:38 -0500

    2 files modified, 1 added

---
+++ src/Swiper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+const Swiper1 = () => {
+  return (
+    <div>
+      <Swiper>
+        <SwiperSlide>slide1</SwiperSlide>
+        <SwiperSlide>slide2</SwiperSlide>
+        <SwiperSlide>slide3</SwiperSlide>
+        <SwiperSlide>slide4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
