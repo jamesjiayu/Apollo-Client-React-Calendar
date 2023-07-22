e29799a238e39652873d5e63a093a3ed955040f4
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-07-22 07:23:26 -0500

    1 file modified

--- src/Swiper.js
+++ src/Swiper.js
@@ -1,17 +1,37 @@
 import React from 'react';
+
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
 import 'swiper/css';
 
-const Swiper1 = () => {
+export default () => {
   return (
-    <div>
-      <Swiper className="mySwiper">
-        <SwiperSlide>slide1</SwiperSlide>
-        <SwiperSlide>slide2</SwiperSlide>
-        <SwiperSlide>slide3</SwiperSlide>
-        <SwiperSlide>slide4</SwiperSlide>
-      </Swiper>
-    </div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   );
 };
-export default Swiper1;
+
+// const Swiper1 = () => {
+//   return (
+//     <div>
+//       <Swiper className="mySwiper">
+//         <SwiperSlide>slide1</SwiperSlide>
+//         <SwiperSlide>slide2</SwiperSlide>
+//         <SwiperSlide>slide3</SwiperSlide>
+//         <SwiperSlide>slide4</SwiperSlide>
+//       </Swiper>
+//     </div>
+//   );
+// };
+// export default Swiper1;
